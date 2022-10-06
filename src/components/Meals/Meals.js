6a5682b8c68a8f1fc6f02479d70ae3d5e028aa78
Dharmesh2501 <dharmeshp2003@gmail.com>
2022-10-06 06:02:36 -0700

    first commit

---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,13 @@
+import { Fragment } from 'react';
+
+import MealsSummary from './MealsSummary';
+import AvailabelMeals from './AvailabelMeals';
+
+const Meals = () =>{
+return <Fragment>
+    <MealsSummary/>
+    <AvailabelMeals/>
+</Fragment>
+};
+
+export default Meals;
